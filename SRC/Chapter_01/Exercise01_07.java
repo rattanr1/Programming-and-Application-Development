3a50c2c3b31839d80ad196288a7b7f0ae0c3e4db
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 00:46:08 -0600

    Update Exercise01_07.java

--- SRC/Chapter_01/Exercise01_07.java
+++ SRC/Chapter_01/Exercise01_07.java
@@ -1,9 +1,20 @@
-package exercise01_07;
-
 /**
+ * Exercise 1.7:
+ *      (Approximate pi) pi can be computed using the following formula:
+ *              4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+ * 
+ *      Write a program that displays the result of:
+                (4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) 
+                    - (1.0/11.0) + (1.0/13.0)));
+
+* Use 1.0 instead of 1 in your program.
+
  *
  * Created by Riyan Rattan
  */
+
+package exercise01_07;
+
 public class Exercise01_07 {
 
     public static void main(String[] args) {
