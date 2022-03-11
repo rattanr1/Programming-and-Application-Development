43dd43b3a2842cda9f51acb4d670d70cd481b747
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 01:02:42 -0600

    Update Exercise02_21.java

--- SRC/Chapter_02/Exercise02_21.java
+++ SRC/Chapter_02/Exercise02_21.java
@@ -1,9 +1,19 @@
-package exercise02_21;
-
 /**
- *
- * @Riyan Rattan
+ *Exercise 2.21:
+
+(Financial application: calculate future investment value) Write a program that
+reads in investment amount, annual interest rate, and number of years, and displays
+the future investment value using the following formula:
+        futureInvestmentValue 
+            = investmentAmount * (1 + monthlyInterestRate)^numberOfYears*12
+            
+For example, if you enter amount 1000, annual interest rate 3.25%, and number
+of years 1, the future investment value is 1032.98.
+ 
+ * By: Riyan Rattan
  */
+
+package exercise02_21;
 
 import java.util.Scanner;
 
