253ef987e7344428fa21857d96b87fdc2d524f63
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 01:13:31 -0600

    Update Exercise03_01.java

--- SRC/Chapter_03/Exercise03_01.java
+++ SRC/Chapter_03/Exercise03_01.java
@@ -1,9 +1,29 @@
-package exercise03_01;
-
 /**
- *
- * @Riyan Rattan
+ * Exercise 3.1(Algebra: solve quadratic equations): 
+ * 
+The two roots of a quadratic equation 
+      ax^2 + bx + c = 0 
+can be obtained using the following formula:  (see book)
+* 
+* 
+b^2 - 4ac is called the discriminant of the quadratic equation. If it is positive, the
+equation has two real roots. If it is zero, the equation has one root. If it is negative,
+the equation has no real roots.
+* 
+Write a program that prompts the user to enter values for a, b, and c and displays
+the result based on the discriminant. If the discriminant is:
+*       positive, display two roots. 
+*       0 (zero), display one root. 
+*       Otherwise, display “The equation has no real roots”.
+* 
+Note that you can use Math.pow(x, 0.5) to compute 2x.
+* 
+* 
+ * By: Riyan Rattan
  */
+
+
+package exercise03_01;
 
 import java.util.Scanner;
 
