ce0130b9e005292d04e73266a2bcd6034e8dbb73
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 00:17:09 -0600

    Add files via upload

---
+++ SRC/Chapter_01/Exercise01_05.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise01_05;
+
+/**
+ *
+ * Created by Riyan Rattan
+ */
+public class Exercise01_05 {
+
+    public static void main(String[] args) {
+    /* Displays calculation for Exercise01_05 */             
+    
+  System.out.println((9.5 * 4.5 - 2.5 * 3.0) / (45.5 - 3.5));
+    
+    }
+    
+}
---
+++ SRC/Chapter_01/Exercise01_07.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise01_07;
+
+/**
+ *
+ * Created by Riyan Rattan
+ */
+public class Exercise01_07 {
+
+    public static void main(String[] args) {
+        /* Displays calculation for Exercise01_07 first part */             
+    System.out.println(4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) 
+            - (1.0/11.0)));
+    
+    
+       /* Another method of calculation for Exercise01_07 first part */ 
+    double result = 4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) 
+            - (1.0/11.0));
+        System.out.println(result);
+
+     
+        /* Displays calculation for Exercise01_07 second part */
+    System.out.println(4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) 
+            - (1.0/11.0) + (1.0/13.0)));   
+    }
+    
+}
