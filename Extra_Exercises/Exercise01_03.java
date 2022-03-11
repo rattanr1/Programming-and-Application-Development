3deafbb102ac6083ea9900012fbbd044efaa88ca
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 12:52:49 -0600

    Add files via upload

---
+++ Extra_Exercises/Exercise01_03.java
@@ -0,0 +1,24 @@
+/*
+ * Exercise 1.3:
+
+ *      (Display a pattern) Write a program that displays the following pattern:
+ *
+ *          J    A  V     V  A
+ *          J   A A  V   V  A A
+ *       J  J  AAAAA  V V  AAAAA
+ *        JJ  A     A  V  A     A
+
+By: Riyan Rattan
+ */
+
+package exercise01_03;
+    public class Exercise01_03 {
+        public static void main(String[] args) {
+       
+        System.out.println("   J    A  V     V  A");
+        System.out.println("   J   A A  V   V  A A");
+        System.out.println("J  J  AAAAA  V V  AAAAA");
+        System.out.println(" JJ  A     A  V  A     A");
+    }
+    
+}
