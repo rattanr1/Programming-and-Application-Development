b9d61e18111ad9f9e70b8e1f4c4b0fde17996036
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 12:38:23 -0600

    Add files via upload

---
+++ Extra_Exercises/Chapter_1_7_Welcome.java
@@ -0,0 +1,29 @@
+/*
+1.7 A Simple Java Program (Listing 1.1 and 1.2:
+
+A Java program is executed from the main method in the class.
+Let’s begin with a simple Java program that displays the message Welcome to Java!
+on the console. (The word console is an old computer term that refers to the text entry and
+display device of a computer. Console input means to receive input from the keyboard, and
+console output means to display output on the monitor.) 
+
+@Riyan Rattan
+*/
+
+package chapter_1_7_welcome;
+
+    public class Chapter_1_7_Welcome {
+        public static void main(String[] args) {
+    
+            
+// Display message Welcome to Java! on the console
+        System.out.println("Welcome to Java!");   
+    
+//Create a space and display next few lines    
+        System.out.println("\nProgramming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
+    
+    }
+    
+}
