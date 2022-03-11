269010c3ca608dca4b47390af1e1bad37bd5e037
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 11:40:12 -0600

    Update Exercise03_17.java

--- SRC/Chapter_03/Exercise03_17.java
+++ SRC/Chapter_03/Exercise03_17.java
@@ -1,9 +1,21 @@
+/*
+*Exercise3.17 (Game: scissor, rock, paper):
+
+(Game: scissor, rock, paper) Write a program that plays the popular scissor-rockpaper
+game. (A scissor can cut a paper, a rock can knock a scissor, and a paper can
+wrap a rock.) The program randomly generates a number 0, 1, or 2 representing
+scissor, rock, and paper. The program prompts the user to enter a number 0, 1, or
+2 and displays a message indicating whether the user or the computer wins, loses,
+or draws.
+ *
+ *By: Riyan Rattan
+*/
+
+
 package exercise03_17;
 
-/**
- *
- * @Riyan Rattan
- */
+
+ 
 import java.util.Scanner;
 public class Exercise03_17 {
 
