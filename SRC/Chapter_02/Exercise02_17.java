558d89d777a25754b5dcb53a9d64a83ec414806d
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-11 00:19:22 -0600

    Add files via upload

---
+++ SRC/Chapter_02/Exercise02_17.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise02_17;
+
+/**
+ *
+ * @Riyan Rattan
+ */
+
+import java.util.Scanner; // Scanner is in the java.util package
+
+public class Exercise02_17 {
+    public static void main(String[] args) {
+  
+    //Create a Scanner object
+    Scanner input = new Scanner(System.in);       
+       
+    //Prompt the user to enter a temperature in Fahrenheit between -58F and 41F
+    System.out.print("Enter temperature in Fahrenheit between -58F and 41F: "); 
+    //Declare Temperature
+    double temp = input.nextDouble();
+    
+    //Prompt the user to enter wind speed (>=2) in miles per hour
+    System.out.print("Enter wind speed (>=2) in miles per hour: ");
+    //Declare speed
+    double speed = input.nextDouble();
+    
+    //Calc formula for the new wind-chill temperature from book
+    double windChill = 35.74 + 0.6215 * temp -    
+    35.75 * Math.pow(speed, 0.16)+
+    0.4275 * temp * Math.pow(speed, 0.16);
+    
+  System.out.print("The wind chill index is" + (double) Math.round (windChill * 100000) / 100000.0);
+    // Display the wind-chilltemperature.
+    
+    }
+    
+}
---
+++ SRC/Chapter_02/Exercise02_21.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise02_21;
+
+/**
+ *
+ * @Riyan Rattan
+ */
+
+import java.util.Scanner;
+
+    public class Exercise02_21 {
+        public static void main(String[] args) {
+            
+            //Create a Scanner object to set up prompt
+            Scanner input = new Scanner(System.in);
+        
+        // Prompt to enter the investment amount    
+        System.out.print("Enter investment amount: ");
+        
+        //Declare investment amount
+        double investmentAmount = input.nextDouble();
+        
+        // Prompt to enter the annual interest rate in percentage
+        System.out.print("Enter annual interest rate in percentage: ");
+        
+        //Declare Monthly Interest Rate
+        double monthlyInterestRate = input.nextDouble();
+        monthlyInterestRate /= 1200;
+        
+        // Prompt to enter number of years
+        System.out.print("Enter number of years: ");
+        
+        //Declare years but as a whole number
+        int years = input.nextInt();
+        
+        // Calculate future investment value as from book
+        double futureInvestmentValue =
+            investmentAmount * Math.pow(1 + monthlyInterestRate, years * 12);
+        
+        
+        // Display future investment value
+        System.out.println("Accumulated value is $" + 
+                (double) Math.round (futureInvestmentValue * 100) / 100.0);
+        
+    }
+    
+}
