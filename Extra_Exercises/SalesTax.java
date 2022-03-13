1b8bd00437efcc09021a19cda297185ff90ee27a
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-12 19:10:50 -0600

    Add files via upload

---
+++ Extra_Exercises/SalesTax.java
@@ -0,0 +1,23 @@
+/*
+Display the sales tax from purchase price with two digits after the decimal point
+
+By: Riyan Rattan
+ */
+
+import java.util.Scanner;
+
+public class SalesTax {
+    public static void main(String[] args) {
+        
+    Scanner input = new Scanner(System.in);        
+    
+    
+    System.out.print("Enter purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+        double tax = purchaseAmount * 0.06;
+        
+    System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);    
+    
+    }
+    
+}
