14d2ec77dbe9ddc59eadb69e5e071e23284d2425
Riyan R <71060227+rattanr1@users.noreply.github.com>
2022-03-15 04:50:34 -0500

    Add files via upload

---
+++ Extra_Exercises/Chapter_04/GuessBirthday.java
@@ -0,0 +1,109 @@
+/*
+Can It Guess Your Birth-Day??
+
+Strings are fundamental in programming. The ability to write programs using strings 
+is essential in learning Java programming. You will frequently use strings to write useful programs. 
+
+You can find out the date of the month when someone was born by asking 
+five questions! The program below will display this.
+
+Each question asks whether the day is in one of the five sets of numbers.
+The input of user don't have to be case sensitive.
+The output should be the correct day of birth!
+
+@Riyan Rattan
+ */
+
+package guessbirthday;
+
+//Don't forget this!
+import java.util.Scanner;
+
+public class GuessBirthday {
+    public static void main(String[] args) {
+        
+  String set1 =
+  "1 3 5 7\n" +
+  "9 11 13 15\n" +
+  "17 19 21 23\n" +
+  "25 27 29 31";
+
+ String set2 =
+ "2 3 6 7\n" +
+ "10 11 14 15\n" +
+ "18 19 22 23\n" +
+ "26 27 30 31";
+
+ String set3 =
+ "4 5 6 7\n" +
+ "12 13 14 15\n" +
+ "20 21 22 23\n" +
+ "28 29 30 31";
+
+ String set4 =
+ "8 9 10 11\n" +
+ "12 13 14 15\n" +
+ "24 25 26 27\n" +
+ "28 29 30 31";
+
+ String set5 =
+ "16 17 18 19\n" +
+ "20 21 22 23\n" +
+ "24 25 26 27\n" +
+ "28 29 30 31";
+
+ int day = 0;
+ 
+ //Initiate the scanner
+ Scanner input = new Scanner(System.in);
+
+// Prompt to answer questions
+ System.out.print("Is your birth day in Set1?\n");
+ System.out.print(set1);
+ System.out.print("\nEnter N for No and Y for Yes: ");
+        char answer = input.next().charAt(0);
+
+ if (answer == 'Y' || answer == 'y')
+    day += 1;
+
+ // Prompt to answer next set of questions
+ System.out.print("\nIs your birth day in Set2?\n");
+ System.out.print(set2);
+ System.out.print("\nEnter N for No and Y for Yes: ");
+        answer = input.next().charAt(0);
+
+if (answer == 'Y' || answer == 'y')
+    day += 2;
+
+ // Prompt the user to answer next set
+ System.out.print("\nIs your birth day in Set3?\n");
+ System.out.print(set3);
+ System.out.print("\nEnter N for No and Y for Yes: ");
+        answer = input.next().charAt(0);
+
+if (answer == 'Y' || answer == 'y')
+    day += 4;        
+
+// Prompt again
+ System.out.print("\nIs your birth day in Set4?\n");
+ System.out.print(set4);
+ System.out.print("\nEnter N for No and Y for Yes: ");
+        answer = input.next().charAt(0);
+
+if (answer == 'Y' || answer == 'y')
+    day += 8;
+
+ // Another one
+ System.out.print("\nIs your birth day in Set5?\n");
+ System.out.print(set5);
+ System.out.print("\nEnter N for No and Y for Yes: ");
+        answer = input.next().charAt(0);
+
+if (answer == 'Y' || answer == 'y')
+    day += 16;
+
+ //Finally display the results :)
+ System.out.println("\nYour birth day is " + day + "! (WHOA!!)");
+ 
+    }
+ }
